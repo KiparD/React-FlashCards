4b2f8de08ee6aab825a8ce2c2c9e497486775b12
KiparD <33526108+KiparD@users.noreply.github.com>
2018-05-04 17:19:07 +0300

    Init commit

---
+++ src/Config/Firebase/db_config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyAqll1hdA0Z7pmkl-8zzgX-vYrN3IcNG3k",
+    authDomain: "flash-cards-app-e3bb9.firebaseapp.com",
+    databaseURL: "https://flash-cards-app-e3bb9.firebaseio.com",
+    projectId: "flash-cards-app-e3bb9",
+    storageBucket: "flash-cards-app-e3bb9.appspot.com",
+    messagingSenderId: "969295532467"
+};
